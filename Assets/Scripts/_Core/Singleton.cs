a74c4ae6d24d97bb76531ac82f865b1710f8e1cc
MuriloSart <89427588+MuriloSart@users.noreply.github.com>
2023-10-20 22:49:29 -0300

    Add new scripts and scene menu

---
+++ Assets/Scripts/_Core/Singleton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance;
+
+    public void Awake()
+    {
+        if (Instance == null)
+            Instance = GetComponent<T>();
+        else
+            Destroy(gameObject);
+    }
+}
+
+    
