99a9367fb95e03e7bebbebac88a8b075f6e457bb
MuriloSart <89427588+MuriloSart@users.noreply.github.com>
2023-10-22 00:51:03 -0300

    Add Attack Animation

--- Assets/Scripts/Player/AttackPlayer.cs
+++ Assets/Scripts/Player/AttackPlayer.cs
@@ -6,19 +6,38 @@ using static System.Runtime.CompilerServices.RuntimeHelpers;
 public class AttackPlayer : MonoBehaviour
 {
     public KeyCode keyCode = KeyCode.R;
+    public Animator animator;
+    private bool attack = false;
+    AnimatorStateInfo stateInfo;
+    float normalizedTime;
 
-    // Update is called once per frame
+    //privates
+    private string _tagEnemy = "Enemy";
+
     void Update()
     {
         
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (Input.GetKey(keyCode))
+        if (Input.GetKeyDown(keyCode))
         {
-
+            
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == _tagEnemy && attack)
+        {
+            collision.gameObject.GetComponent<Enemy>().TakeDamage();
+        }
+              
+    }
 
+    private IEnumerator Attack()
+    {
+        attack = true;
+        animator.SetTrigger("Attack");
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        normalizedTime = stateInfo.normalizedTime;
+        yield return new WaitForSeconds(normalizedTime);
+    }
 }
