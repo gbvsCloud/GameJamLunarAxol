35b16c0caa4301fae13b0f7fd6fb52e841e93e4f
gbvsCloud <gabriielblock@gmail.com>
2023-10-31 12:36:48 -0300

    Added camera script

---
+++ Assets/Prefab/Enemy/EnemyStateMachine.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine : MonoBehaviour
+{
+    EnemyIdleState idleState;
+
+
+}
---
+++ Assets/Scripts/CMCamera.cs
@@ -0,0 +1,18 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMCamera : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    Player player;
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+
+        virtualCamera.LookAt = player.transform;
+        virtualCamera.Follow = player.transform;
+        transform.position = player.transform.position;
+    }
+}
